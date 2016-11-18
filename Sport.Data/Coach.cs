3c9781fdc068d60105a6a139ba9bb2337e27d70a
student <student@KIRP-404-STUD04>
2016-11-18 10:09:16 +0300

    add coach class

---
+++ Sport.Data/Coach.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sport.Data
+{
+    public class Coach
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Citizenship { get; set; }
+    }
+}
